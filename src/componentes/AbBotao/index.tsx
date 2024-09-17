e13bb36409f279c20299dea616c23ae3b0d9e31f
FelipeGabrill <felipegabrielodss@gmail.com>
2024-09-17 16:57:07 -0300

    Evoluindo stories e ajustando estilos

--- src/componentes/AbBotao/index.tsx
+++ src/componentes/AbBotao/index.tsx
@@ -11,23 +11,22 @@ const BotaoEstilizado = styled.button<AbBotaoProps>`
     background: ${(props: AbBotaoProps) => props.tipo === 'primario' ? '#EB9B00' : '#FFF'};
     padding: 16px 32px;
     border: 2px solid #EB9B00;
-    color: $${(props: AbBotaoProps) => props.tipo === 'primario' ? '#FFF' : '#EB9B00'}};
+    color: ${(props: AbBotaoProps) => props.tipo === 'primario' ? '#FFF' : '#EB9B00'}};
     font-size: 20px;
     cursor: pointer;
-    color: $${(props: AbBotaoProps) => props.tipo === 'primario'
+    ${(props: AbBotaoProps) => props.tipo === 'primario'
         ? css`
         &:hover {
             background: #B87900;
             border: 2px solid #B87900;
-        }
-        `
+        }`
         : css`
         &:hover {
             background: #FFF;
             border: 2px solid #B87900;
             color: #B87900
         }`
-    };
+    }
 `
 
 export const AbBotao = ({ texto, onClick, tipo = 'primario' }: AbBotaoProps) => {
--- stories/AbBotao.stories.tsx
+++ stories/AbBotao.stories.tsx
@@ -1,12 +1,24 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 import React from "react";
-import { AbBotao } from "../src";
+import { AbBotao, AbBotaoProps } from "../src";
 
 export default {
     title: 'Componentes/AbBotao',
     component: AbBotao
 } as ComponentMeta<typeof AbBotao>
 
-const Templete: ComponentStory<typeof AbBotao> = () => <AbBotao />
+const Templete: ComponentStory<typeof AbBotao> = (args) => <AbBotao {...args} />
 
 export const Primario = Templete.bind({})
+
+Primario.args = {
+    texto: 'Ab Botão Primário',
+    tipo: 'primario'
+} as AbBotaoProps
+
+export const Secundario = Templete.bind({})
+
+Secundario.args = {
+    texto: 'Ab Botão Secundário',
+    tipo: 'secundario'
+} as AbBotaoProps
