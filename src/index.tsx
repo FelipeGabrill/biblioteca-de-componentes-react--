f7d43ae102210d4afbff0cc8e6fdf79ab81326fd
FelipeGabrill <felipegabrielodss@gmail.com>
2024-09-18 15:18:17 -0300

    Criando o Card

--- src/index.tsx
+++ src/index.tsx
@@ -14,5 +14,6 @@ export const Thing: FC<Props> = ({ children }) => {
   return <div>{children || `the snozzberries taste like snozzberries`}</div>;
 };
 
-export * from './componentes/AbBotao'
-export * from './componentes/AbGrupoOpcoes'
+export * from './components/AbBotao'
+export * from './components/AbGrupoOpcoes'
+export * from './components/Card'
